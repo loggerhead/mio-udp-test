7966118d15670043f25bedd09432eb7feda76502
loggerhead <lloggerhead@gmail.com>
2017-01-08 11:12:22 +0800

    add tasks queue

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 extern crate mio;
 
-use std::slice::from_raw_parts_mut;
 use std::str::FromStr;
 use std::time::Duration;
 use std::net::SocketAddr;
 use std::io::{Result, Error, ErrorKind};
+use std::collections::VecDeque;
 
 use mio::*;
 use mio::udp::UdpSocket;
 
 const BUF_SIZE: usize = 1024;
 
-pub struct Resolver {
-    token: Token,
-    sock: UdpSocket,
-    server: SocketAddr,
-    receive_buf: Option<Vec<u8>>,
+struct Resolver {
+    pub token: Token,
+    pub sock: UdpSocket,
+    pub echo_server: SocketAddr,
+    pub tasks: VecDeque<Vec<u8>>,
 }
 
 fn str2socketaddr(s: &str) -> Result<SocketAddr> {
@@ -23,82 +23,83 @@ fn str2socketaddr(s: &str) -> Result<SocketAddr> {
 }
 
 impl Resolver {
-    fn new(token: Token, server: &str) -> Result<Resolver> {
+    fn new(token: Token, echo_server: &str) -> Result<Resolver> {
         #[cfg(feature = "localhost")]
         let addr = "127.0.0.1:0";
         #[cfg(not(feature = "localhost"))]
         let addr = "0.0.0.0:0";
         let addr = str2socketaddr(addr)?;
-        let server = str2socketaddr(server)?;
+        let echo_server = str2socketaddr(echo_server)?;
         let sock = UdpSocket::bind(&addr)?;
 
         Ok(Resolver {
             token: token,
             sock: sock,
-            server: server,
-            receive_buf: Some(Vec::with_capacity(BUF_SIZE)),
+            echo_server: echo_server,
+            tasks: VecDeque::new(),
         })
     }
 
-    fn send_request(&self, data: &[u8]) -> Result<()> {
-        if let Some(nwrited) = self.sock.send_to(&data, &self.server)? {
-            println!("writed {}/{}", nwrited, data.len());
-        } else {
-            println!("writed nothing");
+    fn add_task(&mut self, _poll: &Poll, data: &[u8]) -> Result<()> {
+        self.tasks.push_back(data.to_vec());
+        #[cfg(feature = "reregister")]
+        self.reregister(_poll, Ready::readable() | Ready::writable())?;
+        Ok(())
+    }
+
+    fn handle_event(&mut self, _poll: &Poll, event: Ready) -> Result<()> {
+        if event.is_error() {
+            return Err(self.sock.take_error()?
+                       .or(Some(Error::new(ErrorKind::Other, "event error")))
+                       .unwrap());
+        }
+
+        if event.is_writable() {
+            if let Some(data) = self.tasks.pop_front() {
+                self.send_request(&data)?;
+            }
+        }
+
+        if event.is_readable() {
+            let data = self.receive_data()?;
+            println!("received[{}]: {:?}", data.len(), data);
+        }
+
+        #[cfg(feature = "reregister")]
+        {
+            let interest = if self.tasks.is_empty() {
+                Ready::readable()
+            } else {
+                Ready::readable() | Ready::writable()
+            };
+            println!("reregistered {:?}", interest);
+            self.reregister(_poll, interest)?;
         }
         Ok(())
     }
 
-    fn receive_data_into_buf(&mut self) -> Result<()> {
-        let mut res = Ok(());
-        let mut buf = self.receive_buf.take().unwrap();
-        // get writable slice from vec
-        let ptr = buf.as_mut_ptr();
-        let cap = buf.capacity();
-        let buf_slice = unsafe { &mut from_raw_parts_mut(ptr, cap) };
-        unsafe {
-            buf.set_len(0);
+    fn send_request(&self, data: &[u8]) -> Result<bool> {
+        match self.sock.send_to(&data, &self.echo_server) {
+            Ok(None) => Err(Error::new(ErrorKind::WouldBlock, "write nothing")),
+            Ok(Some(nwrite)) => Ok(nwrite == data.len()),
+            Err(e) => Err(e),
         }
+    }
 
-        match self.sock.recv_from(buf_slice) {
-            Ok(None) => {}
+    fn receive_data(&mut self) -> Result<Vec<u8>> {
+        let mut buf = [0u8; BUF_SIZE];
+
+        match self.sock.recv_from(&mut buf) {
+            Ok(None) => Err(Error::new(ErrorKind::WouldBlock, "read nothing")),
             Ok(Some((nread, addr))) => {
-                unsafe {
-                    buf.set_len(nread);
-                }
-                println!("received data from {:?}", addr);
+                println!("received {} bytes from {:?}", nread, addr);
+                Ok(buf[..nread].to_vec())
             }
-            Err(e) => res = Err(From::from(e)),
-        }
-        self.receive_buf = Some(buf);
-        res
-    }
-
-    fn handle_events(&mut self, poll: &Poll, events: Ready) -> Result<()> {
-        if events.is_error() {
-            let e = self.sock
-                .take_error()?
-                .or(Some(Error::new(ErrorKind::Other, "event error")))
-                .unwrap();
-            let _ = poll.deregister(&self.sock);
-            self.register(poll)?;
-            Err(e)
-        } else {
-            self.receive_data_into_buf()?;
-
-            if self.receive_buf.as_ref().unwrap().is_empty() {
-                Err(Error::new(ErrorKind::Other, "receive buffer is empty"))
-            } else {
-                let receive_buf = self.receive_buf.take().unwrap();
-                println!("received[{}]: {:?}", receive_buf.len(), receive_buf);
-                self.receive_buf = Some(receive_buf);
-                Ok(())
-            }
+            Err(e) => Err(e),
         }
     }
 
-    fn do_register(&mut self, poll: &Poll, is_reregister: bool) -> Result<()> {
-        let events = Ready::readable();
+    fn do_register(&mut self, poll: &Poll, events: Ready, is_reregister: bool) -> Result<()> {
         let pollopts = if cfg!(feature = "level") {
             PollOpt::level()
         } else {
@@ -108,73 +109,74 @@ impl Resolver {
                 PollOpt::edge()
             }
         };
-        println!("pollopts = {:?}", pollopts);
 
         if is_reregister {
             poll.reregister(&self.sock, self.token, events, pollopts)
-                .map_err(From::from)
         } else {
+            println!("registered ({:?}, {:?})", pollopts, events);
             poll.register(&self.sock, self.token, events, pollopts)
-                .map_err(From::from)
         }
     }
 
-    fn register(&mut self, poll: &Poll) -> Result<()> {
-        self.do_register(poll, false)
+    fn register(&mut self, poll: &Poll, events: Ready) -> Result<()> {
+        self.do_register(poll, events, false)
     }
 
     #[allow(dead_code)]
-    fn reregister(&mut self, poll: &Poll) -> Result<()> {
-        self.do_register(poll, true)
+    fn reregister(&mut self, poll: &Poll, events: Ready) -> Result<()> {
+        self.do_register(poll, events, true)
     }
 }
 
+const ECHO_SERVER: &'static str = "127.0.0.1:9000";
 const TIMEOUT: u64 = 5;
 const RESOLVER_TOKEN: Token = Token(0);
 const TESTS: &'static [&'static str] = &["hi, guys",
                                          "good morning",
                                          "see you later"];
 fn main() {
-    let server = "127.0.0.1:9000";
-    let poll_timeout = Duration::new(TIMEOUT, 0);
-    let mut resolver = Resolver::new(RESOLVER_TOKEN, &server).unwrap();
+    let timeout = Duration::new(TIMEOUT, 0);
+    let mut resolver = Resolver::new(RESOLVER_TOKEN, ECHO_SERVER).unwrap();
     let poll = Poll::new().unwrap();
-    resolver.register(&poll).unwrap();
     let mut events = Events::with_capacity(1024);
+    resolver.register(&poll, Ready::readable()).unwrap();
+
     for hostname in TESTS {
-        match query(hostname, &mut resolver, &poll, &mut events, poll_timeout) {
+        if let Err(e) = resolver.add_task(&poll, hostname.as_bytes()) {
+            println!("add task failed: {}", e);
+        }
+    }
+
+    loop {
+        match query(&mut resolver, &poll, &mut events, timeout) {
+            // Ok(finished) => if finished { break; },
             Err(e) => println!("ERROR: {}", e),
-            _ => {}
+            _ => {},
         }
     }
 }
 
-fn query(hostname: &'static str,
-         resolver: &mut Resolver,
+fn query(resolver: &mut Resolver,
          poll: &Poll,
          events: &mut Events,
-         timeout: Duration) -> Result<()> {
-    println!("<--------- {}", hostname);
-
-    let _ = resolver.send_request(hostname.as_bytes())?;
-
-    // TODO: debug
-    while poll.poll(events, Some(timeout))? == 0 {
-        println!("ERROR: timeout of {}", hostname);
-    }
-
-    for event in events.iter() {
-        println!("{:?}", event);
-        match event.token() {
-            RESOLVER_TOKEN => {
-                #[cfg(feature = "reregister1")]
-                resolver.reregister(&poll)?;
-                resolver.handle_events(&poll, event.kind())?;
-                #[cfg(feature = "reregister2")]
-                resolver.reregister(&poll)?;
+         timeout: Duration) -> Result<bool> {
+    let nevents = poll.poll(events, Some(timeout))?;
+    if nevents == 0 {
+        println!("poll no events");
+    } else {
+        for event in events.iter() {
+            let token = event.token();
+            let kind = event.kind();
+            println!("{:?} => {:?}", token, kind);
+            if token == RESOLVER_TOKEN {
+                resolver.handle_event(poll, kind)?;
             }
-            _ => unreachable!(),
+
+            if kind.is_readable() && resolver.tasks.is_empty() {
+                return Ok(true)
+            }
         }
     }
-    Ok(())
+
+    Ok(false)
 }
